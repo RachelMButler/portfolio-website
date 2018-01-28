87500c4a31b35d1a64848d4bc9be9d64ff288cca
DESKTOP-4QD02HB\Rachel <rachelmbutler@gmail.com>
2018-01-27 17:14:46 -0800

    add work sample descriptions that scroll when the image or title is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 console.log("Welcome to my portfolio site!");
+$(document).ready(function() {
+  $(".project-preview").on("click", function() {
+    $(this).siblings(".project-details").slideToggle();
+  });
+});
