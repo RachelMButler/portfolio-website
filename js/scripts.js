ae54ef82303c32da3134c14aa6e56cd30c71c60c
DESKTOP-4QD02HB\Rachel <rachelmbutler@gmail.com>
2018-02-03 12:22:37 -0800

    change look of website - mistake was made discarding branch so this commit contains all of the changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,1 @@
 console.log("Welcome to my portfolio site!");
-$(document).ready(function() {
-  $(".project-preview").on("click", function() {
-    $(this).siblings(".project-details").slideToggle();
-  });
-});
